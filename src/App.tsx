12eb277fb5c49be9bba11f556b0c3a84d374e6b1
Kristina <kristina@pop-os.localdomain>
2021-01-25 16:22:32 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import VendingMachineContainer from "./containers/vending-machine/VendingMachineContainer";
 import { Provider } from "react-redux";
 import store from "./redux-store/store";
 import MenuContainer from "./containers/menu/MenuContainer";
 import "./index.css";
-import BasketContainer from "./containers/basket/BasketContainer";
 
 const App = () => {
   return (
     <Provider store={store}>
       <MenuContainer />
-      <BasketContainer />
     </Provider>
   );
 };
--- src/containers/menu/MenuContainer.tsx
+++ src/containers/menu/MenuContainer.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import MenuButton from "./MenuButton";
 import MenuWindow from "./MenuWindow";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import BasketContainer from "../basket/BasketContainer";
+import VendingMachineContainer from "../vending-machine/VendingMachineContainer";
 
 const MenuContainer = () => {
   const [openFull, setOpenFull] = useState(false);
 
   return (
-    <>
+    <Router>
       <MenuButton
         onClick={() => setOpenFull(!openFull)}
         id={openFull ? "open" : "close"}
@@ -17,15 +20,32 @@ const MenuContainer = () => {
           <i className="fas fa-times fa-lg" />
         )}
       </MenuButton>
+      <Switch>
+        <Route path="/basket">
+          <BasketContainer />
+        </Route>
+        <Route exact path="/">
+          <VendingMachineContainer />
+        </Route>
+      </Switch>
       {openFull ? (
         <MenuWindow>
           <h2>Vending Machine</h2>
-          <button>Machine</button>
-          <button>Wallet</button>
-          <button>Basket</button>
+          <nav>
+            <button onClick={() => setOpenFull(false)}>
+              <Link to="/">Machine</Link>
+            </button>
+            <button onClick={() => setOpenFull(false)}>
+              Wallet
+              {/* <Link to="/">Wallet</Link> */}
+            </button>
+            <button onClick={() => setOpenFull(false)}>
+              <Link to="/basket">Basket</Link>
+            </button>
+          </nav>
         </MenuWindow>
       ) : null}
-    </>
+    </Router>
   );
 };
 
